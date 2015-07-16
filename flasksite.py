642cf908b18afecbff0303836b2709c5a3067d06
MikeTim <merickson@millenniumbrooklynhs.org>
2015-07-16 10:11:23 -0400

    updated render temp params

--- flasksite.py
+++ flasksite.py
@@ -9,8 +9,6 @@ from flask import render_template
 def root():
 	return render_template("index.html", name = "Project Zero - C-Stuy", title = "Project Zero", Stuff = "Landjaeger picanha tenderloin prosciutto corned beef. Salami ham hock flank jerky, picanha pancetta turducken cow strip steak fatback chicken pork loin capicola. Ribeye tenderloin corned beef hamburger brisket ball tip. Spare ribs frankfurter hamburger jowl pastrami pancetta pork loin chuck kevin short loin pork bresaola beef ribs boudin meatball.")
 
-@module.route('/')
-
 
 if __name__ == "__main__":
 	module.debug = True
