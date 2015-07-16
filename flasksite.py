0710f0c94f9cd9ff3582e93057072ef1ac2e1320
MikeTim <merickson@millenniumbrooklynhs.org>
2015-07-16 09:59:28 -0400

    Added Working Flask Webserver

--- flasksite.py
+++ flasksite.py
@@ -1,15 +1,20 @@
-from fileio_v1 import read_file
+#flask Site
+from utils.fileio_v1 import read_file
 from flask import Flask
-
 module = Flask(__name__)
+from flask import render_template
+
 
 @module.route('/')
 def root():
-	read_file("/static/index.html")
-	return text
+	return render_template("index.html", name = "fdfd")
 
 
 
-	
+
+if __name__ == "__main__":
+	module.debug = True
+	module.run()
+
 
 
